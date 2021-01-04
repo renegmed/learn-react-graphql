74dc44512c34c8284b8ccde2999af09bab9df9c8
rene <renegmed@yahoo.com>
2021-01-04 07:16:48 -0500

    section 6: bonding queries with components

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
-import gql from 'graphql-tage';
+import gql from 'graphql-tag';
+import  { graphql } from 'react-apollo';
+
 
 class SongList extends Component {
-    render() {
+    render() { 
+      console.log(this.props); // will be rendered 2 times. See data loading status in console log e.g. loading true/false, songs
       return(
         <div>
             SongList
@@ -19,4 +22,7 @@ const query = gql`
  } 
 }
 `;
-export default SongList;
+
+// graphql(query) returns a function. It is immediately invocated or called by SongList
+// SongList actually executes the query
+export default graphql(query)(SongList); 
