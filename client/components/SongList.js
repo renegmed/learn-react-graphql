20427c2973d8885e0884393e6634df0b74fdd9fd
rene <renegmed@yahoo.com>
2021-01-04 12:01:28 -0500

    section 9: a quick css breather

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -15,7 +15,7 @@ class SongList extends Component {
     renderSongs() {   
         return this.props.data.songs.map(({id, title}) => {
             return (
-                <li key={id} className="collection=item">
+                <li key={id} className="collection-item">
                     {title}
                     <i
                       className="material-icons"
