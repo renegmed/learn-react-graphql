0e0d3742c584391dd8860708fdd82c69a6c72e55
rene <renegmed@yahoo.com>
2021-01-04 06:47:50 -0500

    section 6: apollo client setup

--- client/index.js
+++ client/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-client';
+import { ApolloProvier } from 'react-apollo';
+
+const client = new ApolloClient({}); // assume server /graphql is available
 
 const Root = () => {
-  return <div>Lyrical</div>
+  return (
+    <ApolloProvier client={client}>
+      <div>Lyrical</div>
+    </ApolloProvier>
+  );
 };
 
 ReactDOM.render(
