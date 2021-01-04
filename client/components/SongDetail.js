6b0d389c261c3c177f431a6ff3bbcdc1d6f7fdb8
rene <renegmed@yahoo.com>
2021-01-04 13:17:22 -0500

    section 10: navigating between screens

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
 import fetchSong from '../queries/fetchSong';
 
 class SongDetail extends Component {
@@ -8,8 +9,9 @@ class SongDetail extends Component {
 
         if (!song) { return <div>Loading....</div>; }
 
-        return(
+        return(           
             <div>
+                 <Link to="/">Back</Link>
                 <h3>{song.title}</h3>
             </div>
         );
